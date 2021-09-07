f648c7df4199c46e12224d4bf4fc20de8c473d62
Keynes Kanno <keyneskanno@gmail.com>
2021-09-07 16:29:36 -0300

    estudando classe abstrata

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,20 +1,7 @@
 
-public class Gerente extends Funcionario {
+public class Gerente extends FuncionarioAutenticavel {
 	
-	private int senha;
-	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
+		
 	public double getBonificacao() {
 		System.out.println("Chamando método de bonificacao do gerente");
 		return getSalario(); //indica que a variável é da classe mae - fica mais facil de entender
